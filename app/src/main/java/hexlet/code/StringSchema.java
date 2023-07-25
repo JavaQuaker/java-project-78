386d2b306fae2effd9b737c5e0dca82c0394d685
JavaQuaker <83884501+JavaQuaker@users.noreply.github.com>
2023-07-25 15:43:49 +0300

    Update StringSchema.java
    update StringSchema

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,30 +1,43 @@
 package hexlet.code;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Predicate;
 
-public class StringSchema {
-    StringSchema() {
+public class StringSchema extends BaseClass {
+    static Map<String, Predicate> check = new LinkedHashMap<>();
 
+    static boolean required;
+    private int minLength;
+    private String contains;
+
+    @Override
+    public StringSchema string() {
+        return null;
     }
-    static String str;
 
     // проверка на пустоту
-    public static Boolean required(String string) {
-      if (Validator.isValid(str).isEmpty()) {
-          return false;
-      }
-      return true;
-    }
+   public StringSchema required() {
+        this.required = true;
+        check.put("required", r -> Objects.equals(r, null));
+        return this;
+   }
     // ограничение минимальной длины
-    public static Boolean minLength(int minLength) {
-        if (str.length() < minLength) {
-            return false;
-        }
-       return true;
+    public StringSchema minLength(int minLength) {
+        this.minLength = minLength;
+       check.put("minLength", (Predicate<String>) str -> str.length() >= minLength);
+        return this;
     }
     // ограничение содержимого строки
-    public static Boolean contains(String string) {
-        if (str.contains(string)) {
-            return true;
-        }
-        return false;
+    public StringSchema contains(String contains) {
+        this.contains = contains;
+        check.put("contains", (Predicate<String>) str -> str.contains(contains));
+        return this;
     }
+
+    @Override
+    public Boolean isValid(Object object) {
+        return true;
+    }
+
 }
