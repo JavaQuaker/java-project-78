081f3ff0fed9ceb26fd236d2738f14e32db8e075
Alexey <leha_s78@mail.ru>
2023-07-31 21:43:05 +0300

    6 test

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,4 +1,5 @@
 package hexlet.code;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 //import java.util.Objects;
@@ -15,9 +16,10 @@ public class StringSchema extends BaseClass {
 
     // проверка на пустоту
     public StringSchema required() {
-        check.put("required", str -> false);
+        check.put("required", str -> str instanceof String && ((String) str).isEmpty());
         return this;
     }
+
     // ограничение минимальной длины
     public StringSchema minLength(int min) {
         check.put("minLength", str -> str.toString().length() >= min);
@@ -25,30 +27,31 @@ public class StringSchema extends BaseClass {
         return this;
 
     }
+
     // ограничение содержимого строки
     public StringSchema contains(String string) {
         check.put("contains", str -> str.toString().contains(string));
         count = string;
         return this;
     }
+
     public Boolean isValid(Object object) {
-        if (!check.containsKey("required") & (check.get("minLength") == null) | (check.get("contains") == null)) {
+
+        if ((!check.containsKey("required")) & ((check.get("minLength")) == null || (check.get("contains") == null))) {
             return true;
-        }
-        if (check.containsKey("required") & (check.get("minLength") == null)
-                | (check.get("contains") == null)) {
+        } else if ((check.containsKey("required")) & ((check.get("minLength") == null)
+                & (check.get("contains") == null))) {
             return false;
+
+
+        } else if (check.containsKey("minLength") && (check.containsKey("contains"))) {
+            return check.get("minLength").test(object) && check.get("contains").test(object);
+        } else if (check.containsKey("minLength")) {
+            return check.get("minLength").test(object);
+        } else if (check.containsKey("contains")) {
+            return check.get("contains").test(object);
         }
-
         return true;
-
-
-//        if (check.get("minLength").test(stringLength)) {
-//            return true;
-//        }
-//        if (check.get("contains").test(count)) {
-//            return true;
-
     }
 }
 
--- app/src/test/java/hexlet/code/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/StringSchemaTest.java
@@ -8,40 +8,64 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringSchemaTest {
 
 
-
-//    @Test
-//    public void isValidTest() {
-//        Validator v = new Validator();
-//        StringSchema schema = v.string();
-//        assertThat(schema.isValid("")).isTrue();
-//    }
     @Test
-    public void isValidTestNull() {
+    public void isValidTest() {
         Validator v = new Validator();
         StringSchema schema = v.string();
+        assertThat(schema.isValid("")).isTrue();
         assertThat(schema.isValid(null)).isTrue();
-
     }
+
+
     @Test
     public void requiredTest() {
         Validator v = new Validator();
         StringSchema schema = v.string();
         schema.required();
         assertThat(schema.isValid(null)).isFalse();
-//        assertThat(schema.isValid("")).isFalse();
-//        assertThat(schema.isValid(5)).isFalse();
-//        assertThat(schema.isValid("what does the fox say")).isTrue();
-//        assertThat(schema.isValid("hexlet")).isTrue();
+        assertThat(schema.isValid("")).isFalse();
+        assertThat(schema.isValid(5)).isFalse();
+        assertThat(schema.isValid("what does the fox say")).isFalse();
+        assertThat(schema.isValid("hexlet")).isFalse();
+        assertThat(schema.contains("wh").isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains("what").isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains("whatthe").isValid("what does the fox say")).isFalse();
+    }
 
-//    }
-//    @Test
-//    public void containsTest() {
-//        Validator v = new Validator();
-//        StringSchema schema = v.string();
-//        schema.required();
-//        assertThat(schema.contains("wh").isValid("what does the fox say")).isTrue();
-//        assertThat(schema.contains("what").isValid("what does the fox say")).isTrue();
-//        assertThat(schema.contains("whatthe").isValid("what does the fox say")).isFalse();
-//
+    @Test
+    public void requiredTestMinLength() {
+        Validator v = new Validator();
+        StringSchema schema = v.string().minLength(5);
+        schema.required();
+        assertThat(schema.isValid("qwert")).isTrue();
+    }
+
+    @Test
+    public void requiredTestContains() {
+        Validator v = new Validator();
+        StringSchema schema = v.string().contains("qwe");
+        schema.required();
+        assertThat(schema.isValid("qwerty")).isTrue();
+    }
+
+    @Test
+    public void requiredTestMinLengthContains() {
+        Validator v = new Validator();
+        StringSchema schema = v.string().minLength(6).contains("qwe");
+        schema.required();
+        assertThat(schema.isValid("qwerty")).isTrue();
+    }
+
+
+    @Test
+    public void containsTest() {
+        Validator v = new Validator();
+        StringSchema schema = v.string();
+        schema.required();
+        assertThat(schema.contains("wh").isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains("what").isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains("whatthe").isValid("what does the fox say")).isFalse();
+
     }
 }
+
